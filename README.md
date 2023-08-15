252289a46a100a44565d7ac3524b23c23443a8ee
Pablo <p.guatibonza@uniandes.edu.co>
2023-08-15 12:33:27 -0500

    problema1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ISIS4208-Analisis-Algoritmos
+
+Problema 1 :  El problema 1 tiene como parametro de entrada un grafo, el cual está en forma de un diccionario o set, sin embargo, el programa funciona como si la entrada fuera una lista de listas. Se dejó de esta manera para que se pudieran ver correctamente los nombres de los usuarios de Facebook. El grafo se encuentra en el archivo "problema1.txt" como ejemplo para que se puedan recrear otros grafos. Lo unico necesario para correr el programa es editar el archivo ya mencionado, el cual solo recibe un grafo a la vez
---
+++ tarea1/problema1/problema1.py
@@ -0,0 +1,38 @@
+def six_degrees(graph):
+    initial_node=next(iter(graph))
+
+    visited=set()
+    queue=[]
+    queue.append(initial_node)
+
+    contador=6
+
+    while queue and contador > 0:
+        node=queue.pop()
+        for i in graph[node]:
+            if i not in visited:
+                visited.add(i)
+                queue.append(i)
+        contador-=1
+    return len(visited)==len(graph)
+
+#lee la entrada a partir de un archivo y crea el grafo
+def read_graph(file):
+    graph = {}
+    with open(file, 'r') as f:
+        for line in f:
+            line=line.replace(':',',' ).replace('\n','')
+            lista=line.split(',')
+            key=lista[0]
+            val=lista[1:]
+            graph[key]=val
+    return graph
+
+def main():
+    graph =read_graph('problema1.txt')
+    print("Para el grafo : ",graph)
+    if six_degrees(graph):
+        print('La teoria de los 6 grados de separacion es cierta')
+    else:
+        print('La teoria de los 6 grados de separacion no es cierta')
+main()
