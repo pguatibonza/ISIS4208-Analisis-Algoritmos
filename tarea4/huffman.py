234b7265643a7d5ff779fe5aa935fc01b6fb229a
pguatibonza <p.guatibonza@uniandes.edu.co>
2023-09-14 17:32:39 -0500

    huffman y shannon inicial

---
+++ tarea4/huffman.py
@@ -0,0 +1,58 @@
+import heapq
+#Representa un nodo en el arbol de huffman
+class Node: 
+    def __init__(self, character, frecuency):
+        self.character = character
+        self.frecuency = frecuency
+        self.left = None
+        self.right = None
+
+    def __lt__(self, otro):
+        return self.frecuency < otro.frecuency
+
+def huffman_tree(frequencies):
+    #Priority queue for keeping the nodes sorted
+    priority_queue=[Node(character,frecuency)for character, frecuency in frequencies.items()]
+    heapq.heapify(priority_queue)
+
+    #Building the huffman tree
+    while(len(priority_queue)>1):
+        node1=heapq.heappop(priority_queue)
+        node2=heapq.heappop(priority_queue)
+
+        new_node=Node(None,node1.frecuency+node2.frecuency)
+        new_node.left=node1
+        new_node.right=node2
+
+        heapq.heappush(priority_queue,new_node)
+
+    return priority_queue[0]
+def code_words(huffman_tree:Node,current, coded):
+    if huffman_tree.character is not None:
+        coded[huffman_tree.character]=current
+    if huffman_tree.left is not None:
+        code_words(huffman_tree.left,current+"0",coded)
+    if huffman_tree.right is not None:
+        code_words(huffman_tree.right,current+"1",coded)
+    return coded
+        
+
+def compress(text):
+    frequencies={}
+    #counting the frequency of each character
+    for char in text:
+        if char in frequencies:
+            frequencies[char]+=1
+        else:
+            frequencies[char]=1
+
+    tree= huffman_tree(frequencies)
+
+    coded={}
+    current=""
+    coded=code_words(tree,current,coded)
+
+    print(coded)
+
+compress("asjdajdjasjdajsdajdjpppqow")
+
---
+++ tarea4/shannon.py
@@ -0,0 +1,58 @@
+
+def compress(text):
+    frequencies={}
+    #counting the frequency of each character
+    for char in text:
+        if char in frequencies:
+            frequencies[char]+=1
+        else:
+            frequencies[char]=1
+
+    #sorting the characters according to their frequency
+    frequencies=sorted(frequencies.items(),key=lambda x:x[1],reverse=True)
+    print(frequencies)
+
+    codewords={}
+    for letter,frecuency in frequencies:
+        codewords[letter]=''
+
+    codewords=shannon_fano(codewords,frequencies)
+    print(codewords)
+
+
+    
+def shannon_fano(codewords,frecuencies):
+
+    length=sum(tuple[1] for tuple in frecuencies)
+    half=round(length/2)
+    
+    if len(frecuencies)>1:
+        flag=0
+        i=0
+        #identifies the characters that will be encoded with 0
+        for letter,frecuency in frecuencies:
+            flag+=frecuency
+            i+=1
+            if flag>=half:
+                break
+        #assigns the codewords
+        for j in range(i):
+            codewords[frecuencies[j][0]]+='0'
+        for j in range(i,len(frecuencies)):
+            codewords[frecuencies[j][0]]+='1'
+        #recursion
+        codewords=shannon_fano(codewords,frecuencies[:i])
+        codewords=shannon_fano(codewords,frecuencies[i:])
+        return codewords
+    else:
+        return codewords
+
+
+"""
+Bits esperados : 
+Total bits :
+Entropía :
+"""
+
+
+compress("hola mundoooopppasdasdas")
